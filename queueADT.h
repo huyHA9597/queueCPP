51a54cb0038d33a3211a71a1063ac086e91312a3
huyHA9597 <haanhuy5@gmail.com>
2020-05-03 20:12:34 +0700

    first commit

---
+++ queueADT.h
@@ -0,0 +1,16 @@
+#ifndef H_QUEUEADT
+#define H_QUEUEADT
+
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+template <class Type>
+class queueADT
+{
+public:
+    virtual bool 
+};
+
+#endif
